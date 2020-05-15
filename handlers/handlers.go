8ac6374e21adce893a78d32db417c8ebb3577e15
Andrew Dorokhin <andrew@dorokhin.moscow>
2020-05-15 09:42:43 +0700

    handle json encode error

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"github.com/dorokhin/sample-api/models"
+	"log"
 	"net/http"
 )
 
@@ -11,5 +12,8 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 	users = append(users, models.User{ID: 1, FirstName: "Andrew", LastName: "Dorokhin", Email: "andrew@dorokhin.moscow"})
 	users = append(users, models.User{ID: 2, FirstName: "Vasily", LastName: "Gorev", Email: "vasya_gor@email.com"})
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
+	err := json.NewEncoder(w).Encode(users)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
